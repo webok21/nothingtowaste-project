cc178f27d33e7492a0d585603df22a9689626f51
kommwirt <dw@kommunikationsfachwirt.de>
2021-08-17 09:58:38 +0200

    Basic CSS and Home Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,22 @@ import {
   Route
 } from "react-router-dom";
 import './App.scss';
-import Home from './components/Home'
-import Marketplace from './components/Home'
-import AddProduct from './components/AddProduct'
-import ProductDetail from './components/ProductDetail'
-import ProductSold from './components/ProductSold'
-import Wishlist from './components/Wishlist'
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+import Home from './components/Home/Home'
+import Marketplace from './components/Marketplace/Marketplace'
+import AddProduct from './components/AddProduct/AddProduct'
+import ProductDetail from './components/ProductDetail/ProductDetail'
+import ProductSold from './components/ProductSold/ProductSold'
+import Wishlist from './components/WishList/Wishlist'
+import Nav from "./components/Navigation/Nav";
+import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
 
     <Router>
-
-
       <Nav />
-
+      
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/marketplace" component={Marketplace} />
