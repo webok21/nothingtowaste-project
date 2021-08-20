f2fab3f686a966e9c3283487284627070503c486
makaveli <wlsp@outlook.de>
2021-08-20 15:27:39 +0200

    accept changes

--- frontend/src/components/Navigation/Nav.js
+++ frontend/src/components/Navigation/Nav.js
@@ -39,26 +39,35 @@ const Nav = () => {
                 <div id="logo">
                     <Link to="/"><li>nothingtotrash</li></Link>
                 </div>
-                <ul>
+                <ul className="ul lgout">
                     <Link to="/marketplace"><li>Marktplatz</li></Link>
                     <Link to="/"><li>Über uns</li></Link>
-                    <Link to="/addproduct"><li>Füge ein Artikel hinzu</li></Link>
+                    {/* <Link to="/addproduct"><li>Füge ein Artikel hinzu</li></Link>
                     <Link to="/productsold"><li>Bereits verkauft</li></Link>
-                    <Link to="/wishlist"><li>Meine Wunschliste</li></Link>
+                    <Link to="/wishlist"><li>Meine Wunschliste</li></Link> */}
                 </ul>
-                <div className="toolbar">
-                    {user?.result ? (
-                        <div className="profile">
-                            <Avatar className="userName" >{user?.result.name.charAt(0)}</Avatar>
-                            <button className="btn logout" onClick={logout}>Logout</button>
+
+                {user?.result ? (
+                    <>
+                        <ul className="ul lgin">
+                            <Link to="/addproduct"><li>Füge ein Artikel hinzu</li></Link>
+                            <Link to="/productsold"><li>Bereits verkauft</li></Link>
+                            <Link to="/wishlist"><li>Meine Wunschliste</li></Link>
+                        </ul>
+                        <div className="toolbar">
+                            <div className="profile">
+                                <Avatar className="userName" >{user?.result.name.charAt(0)}</Avatar>
+                                <button className="btn logout" onClick={logout}>Logout</button>
+                            </div>
                         </div>
-                    ) : (
-                        <>
-                            <Button className="btn login" type="submit"  component={Link} to="/auth/login" >Log In</Button>
-                            <Button className="btn register" type="submit"  component={Link} to="/auth/register" >Registriere Dich</Button>
-                        </>
-                    )}
-                </div>
+                    </>
+                ) : (
+                    <>
+                        <Button className="btn login" type="submit" component={Link} to="/auth/login" >Log In</Button>
+                        <Button className="btn register" type="submit" component={Link} to="/auth/register" >Registriere Dich</Button>
+                    </>
+                )}
+
             </nav>
         </header>
     );
