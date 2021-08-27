05407d8af08909c91e010525be78365950945db4
makaveli <wlsp@outlook.de>
2021-08-27 11:04:51 +0200

    connecting heroku to SAP

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'https://nothingtotrash.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://nothingtotrash.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
